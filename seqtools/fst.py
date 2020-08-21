a05214de6f9238c56e6abff77f52c7f240f1aad7
Jonathan D. Jones <jdjones@jhu.edu>
2020-08-21 17:46:47 -0400

    implemented bare-bones CRF training capabilities

--- seqtools/fst.py
+++ seqtools/fst.py
@@ -38,8 +38,83 @@ class UnhashableFstIntegerizer(FstIntegerizer, utils.UnhashableIntegerizer):
     pass
 
 
-# -=( LIBRARY FUNCTIONS )==-----------------------------------------------------
-def fromArray(weights, final_weight=None, semiring=None, input_labels=None, output_labels=None):
+def Lattice(object):
+    def __init__(self, transition_weights=None, final_weights=None, update_method='gradient'):
+        transition_weights = transition_weights[None, ...]
+        transition_weights = transition_weights.reshape(transition_weights.shape[0], -1)
+
+        self._transition_weights = transition_weights
+        self._final_weights = final_weights
+        if update_method == 'gradient':
+            self._updateWeights = gradientStep
+
+    def fit(self, train_samples, train_labels, observation_scores=None, num_epochs=1):
+        if observation_scores is None:
+            observation_scores = self.score(train_samples)
+
+        observation_scores = observation_scores.reshape(transition_weights.shape[0], -1)
+
+        losses = []
+        for i in num_epochs:
+            batch_loss, batch_grad = batchLoss(
+                train_labels, observation_scores, self._transition_weights, self._final_weights
+            )
+
+            self._transition_weights = self._updateWeights(self._transition_weights, batch_grad)
+            losses.append(batch_loss)
+        return np.array(losses)
+
+    def predict(self, test_samples, observation_scores=None):
+        if observation_scores is None:
+            observation_scores = self.score(test_samples)
+      
+        all_preds = []
+        for score_seq in observation_scores:
+            observation_scores = observation_scores.reshape(transition_weights.shape[0], -1)
+            observation_lattice = fromArray(observation_scores)
+            transition_fst = fromArray(transition_weights)
+
+            decode_graph = openfst.compose(observation_lattice, transition_fst)
+            pred_labels = viterbi(decode_graph)
+
+            all_preds.append(pred_labels)
+
+        return all_preds
+
+    def score(self, train_samples):
+        raise NotImplementedError()
+
+
+# -=( MISC UTILS )==-----------------------------------------------------------
+def isLattice(fst):
+    for state in fst.states():
+        input_labels = arc.ilabel for arc in fst.arcs(state)
+        input_label = input_labels[0]
+        if not all(i == input_label for i in input_labels):
+            return False
+    return is_lattice
+
+
+def iteratePaths(fst):
+    paths = [tuple(fst.arcs(fst.start()))]
+    while paths:
+        path = paths.pop()
+        state = path[-1].nextstate
+        if state == -1:
+            yield path
+
+        for arc in fst.arcs(state):
+            new_path = path + (arc,)
+            paths.append(new_path)
+
+
+def outputLabels(fst):
+    for path in iteratePaths(fst):
+        yield tuple(arc.olabel for arc in path)
+
+
+# -=( CREATION AND CONVERSION )==----------------------------------------------
+def fromArray(weights, final_weight=None, arc_type=None, input_labels=None, output_labels=None):
     """ Instantiate a state machine from an array of weights.
 
     Parameters
@@ -47,10 +122,10 @@ def fromArray(weights, final_weight=None, semiring=None, input_labels=None, outp
     weights : array_like, shape (num_inputs, num_outputs)
         Needs to implement `.shape`, so it should be a numpy array or a torch
         tensor.
-    final_weight : semirings.AbstractSemiringWeight, optional
-        Should have the same type as `semiring`. Default is `semiring.zero`
-    semiring : {'standard', 'log'}, optional
-        Default is 'standard' (ie the tropical semiring)
+    final_weight : arc_types.AbstractSemiringWeight, optional
+        Should have the same type as `arc_type`. Default is `arc_type.zero`
+    arc_type : {'standard', 'log'}, optional
+        Default is 'standard' (ie the tropical arc_type)
     input_labels :
     output_labels :
 
@@ -61,26 +136,26 @@ def fromArray(weights, final_weight=None, semiring=None, input_labels=None, outp
         they left, and output labels corresponding to the state they entered.
     """
 
-    if len(weights.shape) == 3:
-        is_acceptor = False
-    elif len(weights.shape) == 2:
-        is_acceptor = True
+    if weights.ndim == 3:
+        is_lattice = False
+    elif weights.ndim == 2:
+        is_lattice = True
     else:
         raise AssertionError(f"weights have unrecognized shape {weights.shape}")
 
-    if semiring is None:
-        semiring = 'standard'
-
-    if input_labels is None:
-        input_labels = tuple(str(i) for i in range(weights.shape[1]))
+    if arc_type is None:
+        arc_type = 'standard'
 
     if output_labels is None:
-        if is_acceptor:
-            output_labels = input_labels
-        else:
-            output_labels = tuple(str(i) for i in range(weights.shape[2]))
+        output_labels = tuple(str(i) for i in range(weights.shape[1]))
 
-    fst = openfst.VectorFst(arc_type=semiring)
+    if input_labels is None:
+        if is_lattice:
+            input_labels = tuple(str(i) for i in range(weights.shape[0]))
+        else:
+            input_labels = tuple(str(i) for i in range(weights.shape[2]))
+
+    fst = openfst.VectorFst(arc_type=arc_type)
     zero = openfst.Weight.zero(fst.weight_type())
     one = openfst.Weight.one(fst.weight_type())
 
@@ -90,12 +165,12 @@ def fromArray(weights, final_weight=None, semiring=None, input_labels=None, outp
     init_state = fst.add_state()
     fst.set_start(init_state)
 
-    if is_acceptor:
+    if is_lattice:
         prev_state = init_state
         for sample_index, row in enumerate(weights):
             cur_state = fst.add_state()
             for i, weight in enumerate(row):
-                input_label_index = i
+                input_label_index = sample_index
                 output_label_index = i
                 if weight != zero:
                     arc = openfst.Arc(
@@ -124,6 +199,41 @@ def fromArray(weights, final_weight=None, semiring=None, input_labels=None, outp
         fst.set_final(cur_state, openfst.Weight(fst.weight_type(), final_weight)
 
     return fst
+
+
+def toArray(lattice):
+    zero = openfst.Weight.zero(lattice.weight_type())
+    # one = openfst.Weight.one(lattice.weight_type())
+
+    num_states = lattice.num_states()
+    num_outputs = lattice.output_symbols().num_symbols()
+    weights = np.full((num_states, num_outputs), float(zero))
+
+    for state in states:
+        for arc in lattice.arcs(state):
+            weights[state, arc.olabel] = float(arc.weight)
+
+    return weights, fst.weight_type()
+
+
+def fromSequence(seq, arc_type='standard'):
+    fst = openfst.VectorFst(arc_type=arc_type)
+    zero = openfst.Weight.zero(fst.weight_type())
+    one = openfst.Weight.one(fst.weight_type())
+
+    init_state = fst.add_state()
+    fst.set_start(init_state)
+
+    cur_state = init_state
+    for i in seq:
+        next_state = fst.add_state()
+        arc = openfst.Arc(i, i, one, next_state)
+        fst.add_arc(cur_state, arc)
+        cur_state = next_state
+
+    fst.set_final(cur_state, openfst.Weight(fst.weight_type(), one)
+
+    return sequence_acceptor
 
 
 def fromTransitions(
@@ -194,44 +304,6 @@ def fromTransitions(
     return fst
 
 
-def align(scores, label_seq):
-    """ Align (ie segment) a sequence of scores, given a known label sequence.
-
-    NOTE: I don't know if this works with score tables that have more than 9
-        columns.
-
-    Parameters
-    ----------
-    scores : array_like of float, shape (num_samples, num_labels)
-        Log probabilities (possibly un-normalized).
-    label_seq : iterable(string or int)
-        The segment-level label sequence.
-
-    Returns
-    -------
-    aligned_labels : tuple(int)
-    alignment_score : semirings.TropicalSemiringWeight
-        Score of the best path through the alignment graph (possible un-normalized)
-    """
-
-    scores_fst = fromArray(-scores, semiring=semirings.TropicalSemiringWeight)
-    label_fst = leftToRightAcceptor(label_seq, semiring=semirings.TropicalSemiringWeight)
-    aligner = scores_fst.compose(label_fst)
-
-    best_path_lattice = aligner.shortest_path()
-    aligned_labels = best_path_lattice.get_unique_output_string()
-    if aligned_labels is not None:
-        aligned_labels = tuple(int(c) for c in aligned_labels)
-    # NOTE: this gives the negative log probability of the single best path,
-    #   not that of all paths, because best_path_lattice uses the tropical semiring.
-    #   In this case it's fine because we only have one path anyway. If we want
-    #   to marginalize over the k-best paths in the future, we will need to lift
-    #   best_path_lattice to the real or log semiring before calling sum_paths.
-    alignment_score = -(best_path_lattice.sum_paths().value)
-
-    return aligned_labels, alignment_score
-
-
 def leftToRightAcceptor(input_seq, semiring=None, string_mapper=None):
     """ Construct a left-to-right finite-state acceptor from an input sequence.
 
@@ -279,27 +351,138 @@ def leftToRightAcceptor(input_seq, semiring=None, string_mapper=None):
     return acceptor
 
 
-def sequenceFsa(seqs, integerizer=None):
-    if integerizer is None:
-        integerizer = HashableFstIntegerizer(tuple(itertools.chain(*seqs)))
-        seqs = tuple(map(integerizer.integerizeSequence, seqs))
+# -=( CRF/HMM ALGORITHMS )==---------------------------------------------------
+def logprob(observation_weights, gt_labels, transition_weights, final_weights):
+    observation_lattice = fromArray(observation_weights)
+    gt_lattice = fromSequence(gt_label)
+    transition_fst = fromArray(transition_weights)
 
-    sequence_acceptor = FST(
-        semirings.TropicalSemiringWeight, string_mapper=lambda i: str(integerizer[i])
-    ).create_from_string(seqs[0])
+    denominator_graph = openfst.compose(observation_lattice, self._seq_fst)
+    denom_betas, denominator = backward(denominator_graph)
 
-    for seq in seqs[1:]:
-        sequence_acceptor = sequence_acceptor.union(
-            FST(
-                semirings.TropicalSemiringWeight, string_mapper=lambda i: str(integerizer[i])
-            ).create_from_string(seq)
+    numerator_graph = openfst.intersect(gt_lattice, denominator_graph)
+    num_betas, numerator = backward(numerator_graph)
+
+    # numerator and denominator are in the log semiring
+    log_prob = float(numerator) - float(denominator)
+
+    grad = logprob_gradient(denominator_graph, betas=denom_betas)
+
+    return log_prob, grad
+
+
+def logprob_gradient(lattice, alphas=None, betas=None):
+    if alphas is None:
+        alphas = forward(lattice)
+
+    if betas is None:
+        betas = backward(lattice)
+
+    num_states = lattice.num_states()
+    num_outputs = lattice.output_symbols().num_symbols()
+    zero = openfst.Weight.zero(lattice.weight_type())
+    grad = np.full((num_states, num_outputs), float(zero))
+
+    for state in lattice.states:
+        for arc in lattice.arcs(state):
+            w_incoming = alphas[state]
+            w_outgoing = betas[arc.nextstate]
+            w_arc = openfst.times(w_incoming, w_outgoing)
+
+            # key = (state, arc.nextstate, arc.ilabel, arc.olabel)
+            grad[state, arc.olabel] = float(w_arc)
+
+    return grad.sum(axis=0), final_grad
+
+
+def forward(lattice):
+    if lattice.arc_type() != 'log':
+        lattice = openfst.arcmap(lattice, map_type='to_log')
+    alphas = openfst.shortestdistance(lattice)
+    return alphas
+
+
+def backward(lattice):
+    if lattice.arc_type() != 'log':
+        lattice = openfst.arcmap(lattice, map_type='to_log')
+    betas = openfst.shortestdistance(lattice, reverse=True)
+    log_Z = betas[lattice.start()]
+    return betas, log_Z
+
+
+def viterbi(lattice):
+    if lattice.arc_type() != 'standard':
+        lattice = openfst.arcmap(lattice, map_type='to_std')
+
+    shortest_paths = openfst.shortestpath(lattice)
+
+    path_outputs = outputLabels(shortest_paths)
+    if len(path_outputs) != 1:
+        raise AssertionError()
+    output = path_outputs[0]
+    return output
+
+
+def align(scores, label_seq):
+    """ Align (ie segment) a sequence of scores, given a known label sequence.
+
+    NOTE: I don't know if this works with score tables that have more than 9
+        columns.
+
+    Parameters
+    ----------
+    scores : array_like of float, shape (num_samples, num_labels)
+        Log probabilities (possibly un-normalized).
+    label_seq : iterable(string or int)
+        The segment-level label sequence.
+
+    Returns
+    -------
+    aligned_labels : tuple(int)
+    alignment_score : semirings.TropicalSemiringWeight
+        Score of the best path through the alignment graph (possible un-normalized)
+    """
+
+    scores_fst = fromArray(-scores, semiring=semirings.TropicalSemiringWeight)
+    label_fst = leftToRightAcceptor(label_seq, semiring=semirings.TropicalSemiringWeight)
+    aligner = scores_fst.compose(label_fst)
+
+    best_path_lattice = aligner.shortest_path()
+    aligned_labels = best_path_lattice.get_unique_output_string()
+    if aligned_labels is not None:
+        aligned_labels = tuple(int(c) for c in aligned_labels)
+    # NOTE: this gives the negative log probability of the single best path,
+    #   not that of all paths, because best_path_lattice uses the tropical semiring.
+    #   In this case it's fine because we only have one path anyway. If we want
+    #   to marginalize over the k-best paths in the future, we will need to lift
+    #   best_path_lattice to the real or log semiring before calling sum_paths.
+    alignment_score = -(best_path_lattice.sum_paths().value)
+
+    return aligned_labels, alignment_score
+
+
+# -=( TRAINING )==-------------------------------------------------------------
+def gradientStep(weights, gradient, step_size=1e-3):
+    new_weights = weights + step_size * gradient
+    return new_weights
+
+
+def batchLoss(train_labels, observation_scores, transition_scores, final_weights):
+    batch_loss = 0
+    batch_grad = 0
+    for score_seq, label_seq in zip(observation_scores, train_labels):
+        log_prob, grad = logprob(
+            score_seq, label_seq,
+            transition_scores, final_weights
         )
 
-    sequence_acceptor = sequence_acceptor.determinize().minimize()
-    return sequence_acceptor
+        batch_loss += -log_prob
+        batch_grad += -grad
+
+    return batch_loss, batch_grad
 
 
-# -=( HELPER FUNCTIONS )==-----------------------------------------------------
+# -=( MOVE TO ANOTHER MODULE )==-----------------------------------------------
 def smoothCounts(
         edge_counts, state_counts, init_states, final_states,
         init_regularizer=0, final_regularizer=0,
@@ -391,6 +574,7 @@ def countSeqs(seqs):
     return bigram_counts, unigram_counts, initial_counts, final_counts
 
 
+# -=( DEPRECATED )==-----------------------------------------------------------
 def argmax(decode_graph, count=1, squeeze=True):
     """ """
 
